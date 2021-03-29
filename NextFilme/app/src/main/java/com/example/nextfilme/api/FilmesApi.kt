397eb587d917f9a1fecc51200fb1e2d3b314fc6a
Marcos Luciano <marcos3dt@gmail.com>
2021-03-29 17:08:15 -0300

    Criando o metodo para receber o get da aplicaçao

---
+++ NextFilme/app/src/main/java/com/example/nextfilme/api/FilmesApi.kt
@@ -0,0 +1,4 @@
+package com.example.nextfilme.api
+
+interface FilmesApi {
+}
--- NextFilme/app/src/main/java/com/example/nextfilme/ui/listaFilmes/MainActivity.kt
+++ NextFilme/app/src/main/java/com/example/nextfilme/ui/listaFilmes/MainActivity.kt
@@ -2,39 +2,21 @@ package com.example.nextfilme.ui.listaFilmes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.nextfilme.R
 import com.example.nextfilme.data.model.Filme
 
 class MainActivity : AppCompatActivity() {
 
-    private val listFilmes = arrayListOf(
-            Filme(
-                id = 0,
-                titulo = "Titanic",
-                descricao = null,
-                dataLancamento = null,
-            ),
-            Filme(
-                id = 0,
-                titulo = "Titanic",
-                descricao = null,
-                dataLancamento = null
-        )
-
-    )
-
-    private fun Filme(id: Int, titulo: String, descricao: Nothing?, dataLancamento: Nothing?) {
-
-    }
-
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        populateList()
-    }
-    private fun populateList (){
-        listFilmes.apply {
-            
-        }
+
+        recyclerView = findViewById(R.id.ListFilme)
     }
 }
+
+
+
+
