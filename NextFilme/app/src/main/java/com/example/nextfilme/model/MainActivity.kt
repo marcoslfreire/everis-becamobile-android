a092e4dd9b339c1617a68537c5c6158be0585d9a
Marcos Luciano <marcos3dt@gmail.com>
2021-03-31 17:13:49 -0300

    Chamada dos metodos na MainActivity e criação do layout para recebers a lista de filmes

--- NextFilme/app/src/main/java/com/example/nextfilme/model/MainActivity.kt
+++ NextFilme/app/src/main/java/com/example/nextfilme/model/MainActivity.kt
@@ -3,12 +3,35 @@ package com.example.nextfilme.model
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nextfilme.R
+import com.example.nextfilme.service.MovieApiInterface
+import com.example.nextfilme.service.MovieApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)}}
+        setContentView(R.layout.activity_main)}
+
+    private fun getMovieData(callback: (List<Movie>) -> Unit){
+        val apiService = MovieApiService.getInstance.create(MovieApiInterface::class.java)
+        apiService.getMovieList().enqueue(object : Callback<MovieRespose> {
+
+            override fun onFailure(call: Call<MovieRespose>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<MovieRespose>, response: Response<MovieRespose>) {
+                return callback(response.body()!!.movies)
+
+            }
+
+        })
+
+    }
+}
 
 
 
--- NextFilme/app/src/main/java/com/example/nextfilme/service/MovieApiInterface.kt
+++ NextFilme/app/src/main/java/com/example/nextfilme/service/MovieApiInterface.kt
@@ -1,10 +1,11 @@
 package com.example.nextfilme.service
 
-import com.example.nextfilme.model.MovieResponse
+
+import com.example.nextfilme.model.MovieRespose
 import retrofit2.Call
 import retrofit2.http.GET
 
-interface MovieApiInterface{
-    @GET("/movie/popular?api_key=cf374a4e08777f0fe280289e3204fc8d")
-    fun getMovieList(): Call<MovieResponse>
+interface MovieApiInterface {
+    @GET("/3/movie/popular?api_key=cf374a4e08777f0fe280289e3204fc8d")
+    fun getMovieList(): Call<MovieRespose>
 }
