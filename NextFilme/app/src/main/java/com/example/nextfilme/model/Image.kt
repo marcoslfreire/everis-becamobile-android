7a72193c1a64db968dbe80d0666be1fdf44762c8
Marcos Luciano <marcos3dt@gmail.com>
2021-04-02 00:01:49 -0300

    criando a classe de mapeamento de retorno da api

--- NextFilme/app/src/main/java/com/example/nextfilme/model/Image.kt
+++ NextFilme/app/src/main/java/com/example/nextfilme/model/Image.kt
@@ -1,4 +1,20 @@
 package com.example.nextfilme.model
 
-class Image {
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Image (
+    @SerializedName("id")
+    val id: String?,
+    @SerializedName("")
+    val image:String?,
+    @SerializedName("")
+    val imageTitle: String?,
+    @SerializedName("")
+    val imageDetalhes: String?
+
+) : Parcelable {
+    constructor() : this("","","","")
 }
