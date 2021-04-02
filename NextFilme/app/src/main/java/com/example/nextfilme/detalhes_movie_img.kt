8badd6e9ff8b37e918d3775c971f898ed33eba48
Marcos Luciano <marcos3dt@gmail.com>
2021-04-01 22:33:55 -0300

    criando o layot de detalhe de filmes

---
+++ NextFilme/app/src/main/java/com/example/nextfilme/detalhes_movie_img.kt
@@ -0,0 +1,11 @@
+package com.example.nextfilme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detalhes_movie_img : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes_movie_img)
+    }
+}
